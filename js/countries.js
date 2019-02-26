1d8c2dce94174fcdc9ac9b4b6004124c6e0fd70b
Beibhinn Keenan <beibhinn.keenan@gmail.com>
2019-02-26 18:39:46 +0000

    Fix issue with spaces in info dropdown and countries script

--- js/countries.js
+++ js/countries.js
@@ -6,10 +6,10 @@ window.onload = function() {
   initCountries();
   let dropdown = $("#country-dropdown");
   for(var i = 0; i < countries.length; ++i) {
-    dropdown.append(`<div data-country=${countries[i]} class="dropdown-data"><a class="dropdown-item">${countries[i]}</a></div>`);
+    dropdown.append(`<div data-country="${countries[i].replace(" ", "_")}" class="dropdown-data"><a class="dropdown-item">${countries[i]}</a></div>`);
   }
   dropdown.children().click(function() {
-    onInfoSubmit(this.dataset.country);
+    onInfoSubmit(this.dataset.country.replace("_", " "));
   });
 };
 
